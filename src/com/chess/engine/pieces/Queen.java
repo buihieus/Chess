34665c3175397798e5c6c18282d711cf7bbacc8f
ArticleFox301 <duc21112003@gmail.com>
2024-07-15 21:15:35 +0700

    Init Board, Chess Pieces

---
+++ src/com/chess/engine/pieces/Queen.java
@@ -0,0 +1,19 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Queen extends Piece{
+    public Queen(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        return List.of();
+    }
+}
